e427f4116097c71730bd0bdca837f542fca54009
ceniklas <mail@ceniklas.se>
2014-04-08 11:22:50 +0200

    expanded DB.java, commented functions

--- src/com/example/timestamp/model/DB.java
+++ src/com/example/timestamp/model/DB.java
@@ -1,6 +1,76 @@
 package com.example.timestamp.model;
 
+import java.util.ArrayList;
+
 public class DB {
+	
+	/*
+	public void set(time: TimePost){
+		
+		//Put time into DB
+	}
+	
+	public void set(times: ArrayList<TimePost>){
+		
+		//Put time into DB
+	}
+	
+	public void set(project: Project){
+		
+		//Put project into DB
+	}
+	
+	public void set(project: Project){
+		
+		//Put project into DB
+	}
+	
+	public TimePost getTime(){
+		
+		//get a TimePostObject
+		return new TimePost();
+	}
+	
+	public ArrayList<TimePost> getTimes(fromDate:Date, toDate:Date, projectId:int){
+		
+		//Get several TimePost objects
+		return new ArrayList<TimePost>();
+	}
+	
+	public ArrayList<TimePost> getTimes(fromDate:Date, toDate:Date){
+		
+		//Get several TimePost objects
+		return new ArrayList<TimePost>();
+	}
+	
+	public ArrayList<TimePost> getUnsignedTimes(){
+		
+		//Get all unsigned TimePost objects
+		return new ArrayList<TimePost>();
+	}
+	
+	public Project getProject(projectId:int){
+	
+		//Get project by id
+		return new Project();
+	}
+	
+	public ArrayList<Project> getProjects(onlyPrivate:Boolean, onlyShared:Boolean, activeSinceDate:Date){
+	
+		//return project by stuff
+		return new ArrayList<Project>();
+	}
+	
+	public void sync(){
+	
+		//Sync remote and local data
+	}
+	
+	
+	
+	
+	
+	*/
 
 	/*
 	 * 
