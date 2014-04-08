66c53090c157b5372407769b1ae2aebe65097718
Jonas Görling <jonas.gorling@gmail.com>
2014-04-08 14:24:15 +0200

    Implemented project class

--- src/com/example/timestamp/model/Project.java
+++ src/com/example/timestamp/model/Project.java
@@ -1,16 +1,33 @@
 package com.example.timestamp.model;
+import java.lang.String;
 
 public class Project {
 
-	/*
-	 * 
-	 * id
-	 * name
-	 * description
-	 * owner
-	 * customer
-	 *  
-	 * 
-	 */
+	  public int id;
+	  public String name;
+	  public String description;
+	  public String owner;
+	  public String customer;
+	  
+	  //Default constructor
+	  public Project()
+	  {
+		  id = -1;
+		  name = "Default Project";
+		  description = "";
+		  owner = "Localhost";
+		  customer = null;
+	  }
+	  
+	  //Constructor
+	  public Project(int ID, String Name, String Description, 
+			  String Owner, String Customer)
+	  {
+		id = ID;
+		name = Name;
+		description = Description;
+		owner = Owner;
+		customer = Customer;
+	  }
 	
 }
