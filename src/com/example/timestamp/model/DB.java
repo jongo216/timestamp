f542b880c52622e0b69df1a567feb3f3812f98ba
ceniklas <mail@ceniklas.se>
2014-04-08 10:38:38 +0200

    added model files AutomationConfig.java, DB.java, Exporter.java, Project.java, TimePost.java, User.java   with short comments for future implementation

---
+++ src/com/example/timestamp/model/DB.java
@@ -0,0 +1,14 @@
+package com.example.timestamp.model;
+
+public class DB {
+
+	/*
+	 * 
+	 * set(time: TimePost); void
+	 * set(times: ArrayList<TimePost>); void
+	 * set(project : Project); void
+	 * 
+	 * 
+	 */
+	
+}
