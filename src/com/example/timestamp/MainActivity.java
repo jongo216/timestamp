1a5265a88018dd440a9d9f7ce168163014c2ac08
unknown <Petpe506@ITN-TP4004-04.ad.liu.se>
2014-04-10 15:57:28 +0200

    merge

---
+++ src/com/example/timestamp/MainActivity.java
@@ -0,0 +1,127 @@
+package com.example.timestamp;
+
+
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+
+
+public class MainActivity extends FragmentActivity implements
+ActionBar.TabListener {
+
+	private ViewPager viewPager;
+	private TabsPagerAdapter mAdapter;
+	private ActionBar actionBar;
+	
+	
+
+    
+	// Tab titles
+	private String[] tabs = { "Översikt", "Rapport", "Redigera" };
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		//Stylear actionbar
+		ActionBar actionBarTop = getActionBar(); 
+		actionBarTop.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM); 
+		actionBarTop.setCustomView(R.layout.actionbar);
+		actionBarTop.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#11ffffff")));
+		
+		//draw actionbar
+		
+
+		// Initilization
+		viewPager = (ViewPager) findViewById(R.id.pager);
+		actionBar = getActionBar();
+		mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+
+		viewPager.setAdapter(mAdapter);
+		actionBar.setHomeButtonEnabled(false);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);		
+
+		// Adding Tabs
+		for (String tab_name : tabs) {
+			actionBar.addTab(actionBar.newTab().setText(tab_name)
+					.setTabListener(this));
+		}
+
+		
+		 // on swiping the viewpager make respective tab selected
+		 
+		viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+			@Override
+			public void onPageSelected(int position) {
+				// on changing the page
+				// make respected tab selected
+				actionBar.setSelectedNavigationItem(position);
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+			}
+		});
+		
+	}
+	
+	
+	
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main_activitybar, menu);
+	    return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// toggle nav drawer on selecting action bar app icon/title
+		// Handle action bar actions click
+		Log.d("felLog", "onOptionItemsSelected");
+		switch (item.getItemId()) {
+		
+		case R.id.action_settings:
+			return true;
+		
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+	
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// on tab selected
+		// show respected fragment view
+		viewPager.setCurrentItem(tab.getPosition());
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+	}
+
+}
---
+++ src/com/example/timestamp/TabsPagerAdapter.java
@@ -0,0 +1,42 @@
+package com.example.timestamp;
+
+import com.example.timestamp.ConfirmReport;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+	
+	
+	public TabsPagerAdapter(FragmentManager fm) {
+		super(fm);
+	}
+
+	@Override
+	public Fragment getItem(int index) {
+
+		switch (index) {
+		case 0:
+			// Change to Start fragment activity
+			return new Start();
+		case 1:
+			// Change to ConfirmReport fragment activity
+			return new ConfirmReport();//new GamesFragment();
+		case 2:
+			// Change to ConfirmReport fragment activity
+			return new ConfirmReport();//new MoviesFragment();
+		}
+
+		return null;
+	}
+
+	@Override
+	public int getCount() {
+		// get item count - equal to number of tabs
+		return 3;
+	}
+
+}
