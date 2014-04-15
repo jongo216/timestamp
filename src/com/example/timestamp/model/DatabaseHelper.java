1038f55ab98802587516d9911d6797b84a9fe884
Simon Jare <simja832@student.liu.se>
2014-04-15 10:58:26 +0200

    hej

--- src/com/example/timestamp/model/DatabaseHelper.java
+++ src/com/example/timestamp/model/DatabaseHelper.java
@@ -125,8 +125,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
     
-    
- 
     // ------------------------ "todos" table methods ----------------//
  
      /**
