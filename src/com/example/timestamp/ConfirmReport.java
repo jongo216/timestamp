f1a959839418989e806664b75bced529a9e55cd3
simbe109 <simbe109@student.liu.se>
2014-04-15 11:04:13 +0200

    Some styling to ConfirmReport

--- src/com/example/timestamp/ConfirmReport.java
+++ src/com/example/timestamp/ConfirmReport.java
@@ -4,6 +4,7 @@ package com.example.timestamp;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -49,6 +50,7 @@ public class ConfirmReport extends Fragment {
 		        View v = super.getView(position, convertView, parent);
 
 		        ((TextView) v).setGravity(Gravity.CENTER);
+		        ((TextView) v).setTextColor(Color.WHITE);
 		        ((TextView) v).setTextSize(25);
 
 		        return v;
@@ -60,6 +62,8 @@ public class ConfirmReport extends Fragment {
 		        View v = super.getDropDownView(position, convertView,parent);
 
 		        ((TextView) v).setGravity(Gravity.CENTER);
+		        ((TextView) v).setBackgroundColor(Color.BLACK);
+		        ((TextView) v).setTextColor(Color.WHITE);
 		        ((TextView) v).setTextSize(18);
 
 		        return v;
