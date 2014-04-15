3fba726588a5aa0d7214c46df30e54dbd4c803eb
ceniklas <mail@ceniklas.se>
2014-04-15 09:24:19 +0200

    added getters and setters user projects

--- src/com/example/timestamp/model/User.java
+++ src/com/example/timestamp/model/User.java
@@ -2,10 +2,45 @@ package com.example.timestamp.model;
 
 public class User {
 
-	/*
-	 * 
-	 * No functions defined yet!
-	 * 
-	 */
+	private String mailAdress;
+	private String userName;
+	private String userPassword;
+
+
+	public User(){
+		setMailAdress("default@default.default");
+	}
+	
+	public User(String mail){
+		setMailAdress(mail);
+	}
+	
+	public String getMailAdress() {
+		return mailAdress;
+	}
+
+	public void setMailAdress(String mailAdress) {
+		this.mailAdress = mailAdress;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	
+	
+
+	
 	
 }
